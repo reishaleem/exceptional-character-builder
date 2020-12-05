de5c8b299a5f9db391707c03a49f2ef204776d9c
Reis Haleem <reishaleem@gmail.com>
2020-12-05 11:36:18 -0500

    add default port for server

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
