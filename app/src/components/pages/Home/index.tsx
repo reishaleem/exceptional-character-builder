4955979afd36e5e30b36c320bf407afaa0c2e2cf
Reis Haleem <reishaleem@gmail.com>
2020-12-06 10:47:24 -0500

    refactor props into variables for future implementation of jwt

--- app/src/components/pages/Home/index.tsx
+++ app/src/components/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "../../organisms/PublicNavbar";
 
 export const Home = () => {
+    const userName = "Reis Haleem";
+    const loggedIn = true;
     return (
         <Navbar
             color="transparent"
-            dropdownMenuLabel="Reis Haleem"
-            userLoggedIn={false}
+            dropdownMenuLabel={userName}
+            userLoggedIn={loggedIn}
         />
     );
 };
