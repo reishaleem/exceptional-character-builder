936f1b9d1ef5479084227acb051af7fd70584f5a
Reis Haleem <reishaleem@gmail.com>
2020-12-25 14:09:43 -0500

    refactor login and register form fields

--- app/src/types/form-types.ts
+++ app/src/types/form-types.ts
@@ -28,3 +28,15 @@ export interface EditNoteFields {
 export interface EditOutlineFields {
     body: string;
 }
+
+export interface LoginFields {
+    email: string;
+    password: string;
+}
+
+export interface RegisterFields {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
